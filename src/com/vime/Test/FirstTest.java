fa0535e3257ee23070dbd45d6d262a9e3eb38308
vMIAMI <1411079322@qq.com>
2020-06-12 02:07:36 +0800

    test2

--- src/com/vime/Test/FirstTest.java
+++ src/com/vime/Test/FirstTest.java
@@ -2,9 +2,6 @@ package com.vime.Test;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class FirstTest {
 
 
@@ -46,6 +43,12 @@ public class FirstTest {
     }
 
 
+
+
+    /******************************************************************************************************************/
+
+
+
     /*
     2. 两数相加
 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -63,15 +66,13 @@ public class FirstTest {
 
     @Test
     public void test2() {
-       /* ListNode n1 = new ListNode(9);
+        ListNode n1 = new ListNode(9);
         ListNode n2 = new ListNode(8);
         n1.next = n2;
 
         ListNode n3 = new ListNode(1);
 
-        addTwoNumbers(n1, n3);*/
-
-        System.out.println(Integer.parseInt("9999999991"));
+        addTwoNumbers(n1, n3);
     }
 
     public class ListNode {
@@ -84,47 +85,23 @@ public class FirstTest {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1.val == 0) return  l2;
-        if(l2.val == 0) return  l1;
-        /*ListNode curNode_1 = l1;
-        int sum_1 = curNode_1.val;
-        while (curNode_1.next != null) {
-            sum_1 *= 10;
-            sum_1 += curNode_1.next.val * 10;
-            curNode_1 = curNode_1.next;
-        }
-
-        ListNode curNode_2 = l2;
-        int sum_2 = curNode_2.val;
-        while (curNode_2.next != null) {
-            sum_2 *= 10;
-            sum_2 += curNode_2.next.val;
-            curNode_2 = curNode_2.next;
-        }*/
-        StringBuffer s1 = new StringBuffer(String.valueOf(l1.val));
-        while (l1.next != null)
+        ListNode ret = new ListNode((l1.val + l2.val) % 10);
+        ListNode curNode = ret;
+        int UpValue = (l1.val + l2.val) / 10;
+        ListNode zeroNode = new ListNode(0);
+        while (l1.next != null || l2.next != null)
         {
-            l1 = l1.next;
-            s1.append(l1.val);
+            l1 = l1.next == null ? zeroNode: l1.next;
+            l2 = l2.next == null ? zeroNode : l2.next;
+            ListNode nextNode = new ListNode((l1.val + l2.val+ UpValue) % 10);
+            curNode.next = nextNode;
+            curNode = nextNode;
+            UpValue = (l1.val + l2.val + UpValue) / 10;
         }
-        long sum_1 = Long.parseLong(s1.reverse().toString());
-
-        StringBuffer s2 = new StringBuffer(String.valueOf(l2.val));
-        while (l2.next != null)
+        if (UpValue > 0)
         {
-            l2 = l2.next;
-            s2.append(l2.val);
-        }
-        long sum_2 = Long.parseLong(s2.reverse().toString());
-
-        long total = sum_1 + sum_2;
-
-        ListNode ret = new ListNode((int)(total % 10));
-        ListNode cur = ret;
-        while ((total /= 10) > 0) {
-            ListNode next = new ListNode((int)(total % 10));
-            cur.next = next;
-            cur = next;
+            ListNode nextNode = new ListNode(UpValue);
+            curNode.next = nextNode;
         }
         return ret;
     }
